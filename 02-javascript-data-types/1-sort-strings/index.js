1533c4291969d19bf9174b6e6ef1c0c8ddaa5781
lev0607 <lev36rus@gmail.com>
2021-08-12 21:32:16 +0300

    second task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,10 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const newArr = [...arr];
+  const order = param === 'asc' ? 'upper' : 'lower';
 
+  newArr.sort((a, b) => a.normalize().localeCompare(b.normalize(), undefined, {caseFirst: order}));
+
+  return param === 'asc' ? newArr : newArr.reverse();
 }
