6cd3ae0e14cd0b5806dbacade7d385a4c0eb4e2f
lev0607 <lev36rus@gmail.com>
2021-08-29 22:11:10 +0300

    refactoring

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  static notification;
 
+  constructor(message = '', {
+    duration = 0,
+    type = '',
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+
+    this.element = element.firstElementChild;
+  }
+
+  show(container) {
+    if (this.notification) {
+      this.clearNotification()
+    }
+    this.timerId = setTimeout(() => {
+        this.remove();
+      }, this.duration
+    )
+    container ? container.append(this.element) : document.body.append(this.element);
+    this.notification = this
+  }
+
+  clearNotification() {
+    clearTimeout(notification.timerId);
+    this.notification.element.remove();
+    this.notification = null;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
